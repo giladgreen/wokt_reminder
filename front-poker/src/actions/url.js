9e17cb6b8b1e48a35d9ea5ed6b9e1542ca0b4cd1
gilad green <gilad.green@autodesk.com>
2021-10-04 09:38:15 +0300

    f

--- front-poker/src/actions/url.js
+++ front-poker/src/actions/url.js
@@ -1,3 +1,3 @@
-const URL_PREFIX = window.location.origin.includes('localhost') ? 'http://localhost:5002' : window.location.origin; //https://wolt-wrapper.herokuapp.com'
+const URL_PREFIX = window.location.origin.includes('localhost') ? 'https://wolt-wrapper.herokuapp.com' : window.location.origin; //https://wolt-wrapper.herokuapp.com' // 'http://localhost:5002'
 
 export default URL_PREFIX
