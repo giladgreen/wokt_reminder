2bc89489d9d55a395c5a6297baf28cf9300be61e
gilad green <gilad.green@autodesk.com>
2021-10-16 14:44:34 +0300

    Update location.js

--- server/services/location.js
+++ server/services/location.js
@@ -6,8 +6,9 @@ const LOCATION_KEY = process.env.LOCATION_KEY ? process.env.LOCATION_KEY : requi
 async function getLocationAddress(req, res, next) {
     try {
         const { lat, lon } = req.query;
-        const { results } = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${LOCATION_KEY}&language=he&region=IL`);
-        const address = results[0].formatted_address;
+        const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${LOCATION_KEY}&language=he&region=IL`);
+        logger.info('response', response)
+        const address = response.body.results[0].formatted_address;
         return res.status(200).send({ address });
     } catch (e) {
         next(e);
