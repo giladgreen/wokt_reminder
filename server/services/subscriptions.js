2f0411b0e2beb06e3e789012bfaf08dbcb7d11c2
gilad green <gilad.green@autodesk.com>
2022-01-18 21:20:07 +0200

    a

--- server/services/subscriptions.js
+++ server/services/subscriptions.js
@@ -7,6 +7,9 @@ const INTERVAL = 30000;//30 sec
 const PRUNE_INTERVAL = 1000 * 60 * 10;//10 minutes
 const AMOUNT = 30;
 const UNITS = 'hours';
+const axios = require('axios');
+const gateId = process.env.GATE_ID;
+const token = process.env.TOKEN;
 
 async function isOpenForDelivery(restaurantName, restaurantId, location) {
    const restaurant = await getSpecificRestaurant(restaurantName, restaurantId, location)
@@ -171,9 +174,22 @@ async function unsubscribe(req, res, next) {
       next(e);
   }
 }
-function onTrigger(req, res, next){
-    sendHtmlMail(`watchtrigger`, ``, 'green.gilad@gmail.com');
-    return res.status(200).send({  });
+async function onTrigger(req, res){
+    const options = {
+        method:'get',
+        url:`https://api1.pal-es.com/v1/bt/device/${gateId}/open-gate?outputNum=1`,
+        headers:{
+            "accept-encoding":"gzip",
+            "connection":"Keep-Alive",
+            "host":"api1.pal-es.com",
+            "user-agent":"okhttp/3.14.7",
+            "x-bt-user-token":token
+        }
+    }
+    const callResult = await axios.get(options.url,options);
+    const {err, msg } = callResult.data;
+    sendHtmlMail(`watchtrigger`, `<div><div>Error:${err} </div><div>Message:${msg} </div></div>`, 'green.gilad@gmail.com');
+    return res.status(200).send({ err, msg });
 }
 setInterval(async()=>{
     const allSubscriptions = await subscriptions.findAll();
